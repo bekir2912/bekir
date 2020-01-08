8c38c685786ae740ef4e6e39af50e59e86be8837
bekir2912 <bekir2912@gmail.com>
2020-01-08 15:47:43 +0500

    Lesson2
    lesson2

---
+++ Leson2/Leson2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Leson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a, b;
+            //Console.WriteLine("First num:");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Second num2:");
+            //b = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Result: " + (a + b));
+            //Console.ReadKey();
+
+            int a = 15;
+            int b = 35;
+            string s = "Your salary is: ";
+
+            Console.WriteLine(s + (a + b));
+            Console.ReadKey();        }
+    }
+}
